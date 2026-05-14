a4faccebc533376e688c687ee56ea945c0fd6dc6
SaraHenriquezz <00160526>
2026-05-14 11:31:47 -0600

    agregar codigo

---
+++ whilee.cpp
@@ -0,0 +1,8 @@
+int numero;
+
+do {
+    cout << "Introduce un numero mayor a 0: ";
+    cin >> numero;
+} while (numero <= 0); 
+
+// El código sigue aquí cuando el usuario pone, por ejemplo, un 5.
